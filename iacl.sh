d5e035cd2b962a718f53946e696b6c25caa2a149
rodrigo <rodrigo.dmorais04@gmail>
2023-02-22 22:38:05 -0300

    Arquivos IaC v.1

---
+++ iacl.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Criando diretórios"
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos de usuários"
+
+groupadd GPR_ADM
+groupadd GPR_VEN
+groupadd GPR_SEC
+
+echo "Criando usuários"
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+
+echo "Especificando permissões dos diretórios"
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Fim"
